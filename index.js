8e7f76b1e958b248b0b2050601376366a81ccfc8
Oleh <opolonetso@gmail.com>
2022-10-29 17:35:02 +0300

    add js code, time go

---
+++ index.js
@@ -0,0 +1,59 @@
+const startBtn = document.querySelector('.start-btn');
+const mainStartBtn = document.querySelector('.main-start-btn');
+
+const mainLapBtn = document.querySelector('.main-lap-btn');
+const lapResetBtn = document.querySelector('lap-reset-btn');
+
+const laps = document.querySelector('.laps');
+
+let milisec = 0;
+let sec = 0;
+let min = 0;
+
+let m = 0;
+let s = 0;
+let ms = 0;
+
+let time = document.querySelector('.time');
+let int = null;
+
+let isTimerStarted = false;
+
+startBtn.addEventListener('click', () => {
+    if (isTimerStarted === false) {
+        int = setInterval(displayTimer, 10);
+    } else {
+        clearInterval(int);
+    }
+    changeStartBtn();
+})
+
+function displayTimer() {
+    milisec++;
+
+    if (milisec >= 99) {
+        sec++;
+        milisec = 0;
+    }
+    if (sec >= 60) {
+        min++;
+        sec = 0;
+    }
+
+    m = min < 10 ? '0' + min : min;
+    s = sec < 10 ? '0' + sec : sec;
+    ms = milisec < 10 ? '0' + milisec : milisec;
+
+    time.innerHTML = `${m}:${s}.${ms}`;
+}
+
+
+function changeStartBtn() {
+    if (isTimerStarted === false) {
+        isTimerStarted = true;
+
+        startBtn.innerHTML = 'Stop';
+        startBtn.classList.add("timerStarted");
+        mainStartBtn.classList.add("timerStartedMain");
+    }
+}
